d73314f2052ddcd9661841097f272aa1bb5c0ecf
Dmitry <49843752+BullitStudio@users.noreply.github.com>
2021-01-07 15:42:33 +0200

    Add Craco + Antd + Customize theme

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+const defaultRules = [
+  "react-app",
+  "eslint:recommended",
+  // any other plugins or custom configuration you'd like to extend from.
+];
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: defaultRules,
+  rules: {
+    "default-case": "warn",
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      plugins: ["@typescript-eslint"],
+      extends: [
+        ...defaultRules,
+        "plugin:@typescript-eslint/recommended",
+        // any other TypeScript specific config (from a plugin, or custom)
+      ],
+      rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-empty-function": "warn",
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-unused-expressions": "warn",
+        // temporarily disabled
+        "no-useless-catch": "off",
+      },
+    },
+  ],
+  settings: {
+    react: {
+      // React version. "detect" automatically picks the version you have installed.
+      version: "detect",
+    },
+  },
+};
